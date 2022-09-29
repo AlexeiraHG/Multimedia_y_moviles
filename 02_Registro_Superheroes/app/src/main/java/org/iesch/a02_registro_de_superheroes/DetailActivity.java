0a41efa97e2ea5029ea109825bd0c20ab78d1a3e
Alexeria <gahg0109@gmail.com>
2022-09-29 17:11:37 +0200

    Funciona app de heroes

--- 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/DetailActivity.java
+++ 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/DetailActivity.java
@@ -2,13 +2,33 @@ package org.iesch.a02_registro_de_superheroes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+
+import org.iesch.a02_registro_de_superheroes.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        ActivityDetailBinding binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // 1 Obtener en un objeto de tipo bundle todos los datos traidos de origen
+        Bundle extras = getIntent().getExtras();
+
+        // 2 Crear variables en los que guardar los valores del "extras"
+        String heroName = extras.getString("superhero_name");
+        String alterego = extras.getString("alterego");
+        String bio = extras.getString("bio");
+        Float rating = extras.getFloat("rating");
+
+        //Cargar variables en los campos de la datail activity
+        binding.nombreHeroeText.setText(heroName);
+        binding.TVAlterEgo.setText(alterego);
+        binding.TVBio.setText(bio);
+        binding.ratingBar.setRating(rating);
+
     }
 }
--- 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/MainActivity.java
+++ 02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_de_superheroes/MainActivity.java
@@ -1,7 +1,9 @@
 package org.iesch.a02_registro_de_superheroes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import org.iesch.a02_registro_de_superheroes.databinding.ActivityMainBinding;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +11,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.saveButton.setOnClickListener(v ->{
+            // 3 Captar informacion en las vista main
+            String superHeroName = binding.nombreHeroEdit.getText().toString();
+            String alterEgo = binding.alterEgoEdit.getText().toString();
+            String bio = binding.bioEdit.getText().toString();
+            Float rating = binding.powerBar.getRating();
+
+            abrirDetailActivity(superHeroName, alterEgo, bio, rating);
+        });
+    }
+
+    private void abrirDetailActivity(String superHeroName, String alterEgo,  String bio, Float rating) {
+
+        // 1 Se crea objeto de tipo intent y se le dice donde se esta y a donde se quiee ir
+        Intent irADetalle = new Intent(this, DetailActivity.class);
+
+        // 3.5 Añadimos al Intent toda la info que queramos pasar, se le pasa una llave (string) y un value
+        irADetalle.putExtra("superhero_name", superHeroName);
+        irADetalle.putExtra("alterego", alterEgo);
+        irADetalle.putExtra("bio", bio);
+        irADetalle.putExtra("rating", rating);
+
+        // 2 Se llama a StarActivity para ir a DetailActivity
+        startActivity(irADetalle);
+
+
     }
 }
